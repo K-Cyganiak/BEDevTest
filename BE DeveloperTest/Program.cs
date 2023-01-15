247f8586724264d56004c7602d3f21f4035633dd
Kamil <kamil.cyganiak@gmail.com>
2023-01-15 23:05:51 +0000

    Umbraco 11 repo init

---
+++ BE DeveloperTest/Program.cs
@@ -0,0 +1,19 @@
+namespace BE_DeveloperTest
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+			=> CreateHostBuilder(args)
+				.Build()
+				.Run();
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureUmbracoDefaults()
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
+					webBuilder.UseStaticWebAssets();
+					webBuilder.UseStartup<Startup>();
+				});
+	}
+}
